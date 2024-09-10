cc98a735e237a73499688d817d7a54dc7fc89f3f
jinwoo <dlwlsdn8022@gmail.com>
2024-09-10 12:44:19 +0900

    first commit

---
+++ src/test/java/jwlee/crudpractice/CrudPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jwlee.crudpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
